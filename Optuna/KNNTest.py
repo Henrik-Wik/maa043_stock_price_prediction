29eaaf5e04354df88730efe026d963f7bef8fa71
HenrikWik97 <henrik.wik@outlook.com>
2023-04-20 13:47:21 +0200

    fixed indent

--- Optuna/KNNTest.py
+++ Optuna/KNNTest.py
@@ -13,14 +13,14 @@ for Stock in Stocks:
     else:
         import preprocessing as pp
 
-        data = pp.download_data(Stock)
-        features, targets, feat_targ_df, feature_names = pp.create_features(data, Stock)
-        train_features, test_features, train_targets, test_targets = pp.time_split(
-            features, targets
-        )
-        scaled_train_features, scaled_test_features, pred_scaler = pp.scale_data(
-            train_features, test_features, targets
-        )
+    data = pp.download_data(Stock)
+    features, targets, feat_targ_df, feature_names = pp.create_features(data, Stock)
+    train_features, test_features, train_targets, test_targets = pp.time_split(
+        features, targets
+    )
+    scaled_train_features, scaled_test_features, pred_scaler = pp.scale_data(
+        train_features, test_features, targets
+    )
 
     best_params = optimize_knn(scaled_train_features, train_targets)
     print("Best hyperparameters:", best_params)
